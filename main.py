1f09c684d361433ed2fd6528bcddc3f1341f4f51
oldmcdonnell <oldmcdonnell@gmail.com>
2023-11-22 20:08:08 -0500

    create PDFs and isolate filename and invoice number.

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from fpdf import FPDF
 from pathlib import Path
 
 filepaths = glob.glob("invoices/*xlsx")
-print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
     pdf.set_font(family="Arial", size=16, style="B")
-    pdf.cell(w=50, h=8, txt="Invoice#")
+    pdf.cell(w=50, h=8, txt=f"Invoice#{invoice_nr}")
+    pdf.output(f"PDFs/{filename}.pdf")
