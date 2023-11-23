ddef3aa0cdb710e1bfc6169624d7db339581e060
oldmcdonnell <oldmcdonnell@gmail.com>
2023-11-22 19:35:48 -0500

    fixed filenames

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import openpyxl
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("invoices/*xlsx")
 print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=50, h=8, txt="Invoice#")
