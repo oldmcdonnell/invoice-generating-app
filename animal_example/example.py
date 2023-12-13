7efd60a3433633aab8635c58d67548a261fa24a4
oldmc <oldmcdonnell@gmail.com>
2023-12-12 23:18:05 -0500

    did example with animals

--- animal_example/example.py
+++ animal_example/example.py
@@ -1,9 +1,25 @@
-import openpyxl
 import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
 
-filepaths = glob.glob("animals/*xlsx")
+filepaths = glob.glob("animals/*txt")
+
+pdf =FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
+    #add a page
+    pdf.add_page()
+
+    #filenames without exntion
+    #title create
+
+    filename = Path(filepath).stem
+    name = filename.title()
+
+    #add the name to the pdf
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=name, ln=1)
+
+#prduce the PDf
+pdf.output("output.pdf")
