901f821e5f99f039cd7b925386e299a849b2f5cf
oldmc <oldmcdonnell@gmail.com>
2023-12-13 12:28:04 -0500

    added date to pdf

--- animal_example/example.py
+++ animal_example/example.py
@@ -7,6 +7,16 @@ filepaths = glob.glob("animals/*txt")
 
 pdf =FPDF(orientation="P", unit="mm", format="A4")
 
+def get_info(filepath=filepaths):
+    with open(filepath, 'r') as file_local:
+        info_local = file_local.readlines()
+    return info_local
+
+def write_info(info_arg, filepath=filepaths):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(info_arg)
+        print(type(info_arg))
+
 for filepath in filepaths:
     #add a page
     pdf.add_page()
@@ -20,6 +30,7 @@ for filepath in filepaths:
     #add the name to the pdf
     pdf.set_font(family="Arial", size=16, style="B")
     pdf.cell(w=50, h=8, txt=name, ln=1)
+#    pdf.cell(w=50, h=50, txt=animal_text, ln=5)
 
 #prduce the PDf
 pdf.output("output.pdf")
--- main.py
+++ main.py
@@ -8,10 +8,19 @@ filepaths = glob.glob("invoices/*xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+
     pdf.set_font(family="Arial", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice#{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice# {invoice_nr}", ln=1)
+
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"date# {date}")
+
+
+
     pdf.output(f"PDFs/{filename}.pdf")
